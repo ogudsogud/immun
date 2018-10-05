bb2028b76449070c12ebe3eca267e6c45895a31d
yoga.wiguna <ogudsogud@gmail.com>
2018-10-05 14:04:51 +0700

    on work

--- src/main/java/com/msg/immun/ImmunApplication.java
+++ src/main/java/com/msg/immun/ImmunApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @EnableAutoConfiguration
-@SpringBootApplication(scanBasePackages = {"com.msg.immun"})
+@SpringBootApplication
+//		(scanBasePackages = {"com.msg.immun"})
 public class ImmunApplication {
 
 	public static void main(String[] args) {
